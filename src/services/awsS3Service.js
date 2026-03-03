3c0a222781842984bba750bb1357cddf1217d923
JohnTech-Loma <jhondejesuscastrovalle@gmail.com>
2026-03-03 17:44:26 -0500

    Subiendo chatbot La estacion

---
+++ src/services/awsS3Service.js
@@ -0,0 +1,28 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
+import config from "../config/env.js";
+
+// Configura AWS SDK v3
+const s3 = new S3Client({
+  region: config.AWS_REGION,
+  credentials: {
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+// Sube la imagen a S3 y retorna la URL pública
+export const uploadToPublicStorage = async (buffer, mimeType = "image/jpeg") => {
+  const fileName = `comprobantes/${uuidv4()}.jpg`;
+  const params = {
+    Bucket: config.AWS_BUCKET_NAME,
+    Key: fileName,
+    Body: buffer,
+    ContentType: mimeType,
+  };
+
+  await s3.send(new PutObjectCommand(params));
+
+  // URL pública
+  return `https://${config.AWS_BUCKET_NAME}.s3.${config.AWS_REGION}.amazonaws.com/${fileName}`;
+};
---
+++ src/services/geminiService.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import config from "../config/env.js";
+
+const GEMINI_API_KEY = config.GEMINI_API_KEY;
+
+const geminiService = async (message) => {	
+
+	try {
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+      {
+        contents: [
+          {
+            parts: [
+              {
+                text: `Actúa como un asesor especializado para el restuarante llamado La Estación Blacks ubicado en el corregimiento de La Loma en el departamento del Cesar, en Colombia. IMPORTANTE: Responde corto, preciso y conciso. Responde la pregunta si la sabes, sino, discúlpate con el cliente diciendo algo como "Uy disculpa! 😬 no se la respuesta, pero seguiré aprendiendo para resolver todas tus inquietudes". No digas: "No se me dio información sobre", "según la información proporcionada". Si te preguntan información sensible o privada como por ejemplo: Quién es el dueño del restaurante, responde de una manera persuasiva y jocosa. Aparenta ser un humano y hablar fluidamente según la conversación, eres el chatbot experto del restaurante Samuelito Restobar. 
+                \n\n${message}`
+              }
+            ]
+          }
+        ]
+      }
+    );
+    // Gemini responde en response.data.candidates[0].content.parts[0].text
+    return response.data.candidates?.[0]?.content?.parts?.[0]?.text || "No se la respuesta";
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    return "Ocurrió un error al consultar la IA.";
+  }
+};
+	
+export default geminiService;
