576601a2df09782fd04245aabcbd2e3c95d17bc5
JohnTech-Loma <jhondejesuscastrovalle@gmail.com>
2026-03-11 17:44:51 -0500

    mas infor cartas

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -19,6 +19,277 @@ INSTRUCCIONES IMPORTANTES:
 - Si el usuario pregunta sobre pedidos, reservas o servicios, ofrece ayuda
 
 INFORMACIÓN DEL RESTAURANTE:
+
+CARTA: 
+ASADOS AL CARBÓN
+
+Carne
+Carne a la brasa, ensalada + papa o patacones.
+Precio: $20.000
+
+Pechuga
+Pechuga a la brasa, ensalada + papa o patacones.
+Precio: $20.000
+
+Pechuga Gratinada
+Pechuga + queso mozzarella, ensaladas + papa o patacones.
+Precio: $25.000
+
+Lomo de Cerdo
+Lomo de cerdo + ensaladas + papa o patacones.
+Precio: $25.000
+
+Chuleta
+Chuleta + ensaladas + papa o patacones.
+Precio: $25.000
+
+Punta Gorda
+Punta gorda + ensaladas + papa o patacones.
+Precio: $25.000
+
+Asado Mixto
+Carne + pechuga + chorizo + ensaladas + papa o patacones.
+Precio: $28.000
+
+Asado Trifásico
+Lomo de cerdo, carne y pechuga + ensalada + papa o patacones.
+Precio: $30.000
+
+ASADOS AL BARRIL
+
+Alitas Tradicionales
+Alitas + papas francesas.
+Precio: $18.000
+
+Alitas BBQ
+Alitas BBQ + papas francesas.
+Precio: $20.000
+
+Costillas de Cerdo BBQ
+Costilla de cerdo BBQ + papas francesas.
+Precio: Desde $25.000
+
+Chicharrón
+Panceta de cerdo + papas francesas.
+Precio: Desde $25.000
+
+Chorizos
+Chorizo + papas francesas.
+Precio: $7.000
+
+SALCHIPAPAS
+
+Porción de Papa
+Papa + queso + salsa.
+Precio: $6.000
+
+Sencilla
+Salchicha, vegetales, salsa y queso.
+Precio: $12.000
+
+Mixta
+Salchicha, chorizo, butifarra, vegetales, salsa y queso.
+Precio: $15.000
+
+Chori-Papa
+Chorizo, vegetales, salsa y queso.
+Precio: $16.000
+
+Ranchera
+Salchicha ranchera, salsa, vegetales y queso.
+Precio: $18.000
+
+Polli-Papa
+Pechuga, vegetales, salsa y queso.
+Precio: $20.000
+
+Especial
+Carne, pechuga, lomo de cerdo, chorizo, butifarra, salchicha, vegetales, salsa y queso.
+Precio: $20.000
+
+Salchi-Plátano
+Palitos de plátano, carne, pechuga, chorizo, butifarra, salchicha, vegetales, salsa y queso.
+Precio: $20.000
+
+Super Estación
+Carne, pechuga, lomo de cerdo, chorizo, butifarra, salchicha, salchicha ranchera, maíz, vegetales, salsa y queso.
+Precio: $25.000
+
+PICADAS
+
+Todas las picadas incluyen: carne, pechuga, lomo de cerdo, chorizo, butifarra, salchicha, vegetales, salsa y queso.
+
+Picada para 2 personas
+Precio: $30.000
+
+Picada para 3 personas
+Precio: $40.000
+
+Picada para 4 personas
+Precio: $50.000
+
+Picada para 5 personas
+Incluye maíz.
+Precio: $60.000
+
+Picada para 6 personas
+Incluye maíz.
+Precio: $70.000
+
+Picada Familiar
+Incluye maíz.
+Precio: $140.000
+
+BURGERS
+
+Sencilla
+Carne artesanal, jamón, queso mozzarella, vegetales + papas y queso.
+Precio: $15.000
+
+De Pollo
+Pechuga asada, jamón, queso mozzarella, vegetales + papas y queso.
+Precio: $18.000
+
+Ranchera
+Carne artesanal, salchicha ranchera, tocineta, jamón, queso mozzarella, vegetales + papa y queso.
+Precio: $20.000
+
+Mixta
+Pechuga asada, carne artesanal, tocineta, jamón, queso mozzarella, vegetales + papa y queso.
+Precio: $25.000
+
+Doble Carne
+2 carnes artesanales, tocineta, queso mozzarella, vegetales + papa y queso, jamón.
+Precio: $25.000
+
+Estacionaria
+Carne artesanal, pollo, salchicha ranchera, chorizo, tocineta, huevo frito, queso mozzarella, vegetales + papa y queso, jamón.
+Precio: $30.000
+
+PATACÓN BURGER
+
+Sencillo
+Carne artesanal, jamón, queso mozzarella, vegetales y queso.
+Precio: $15.000
+
+De Pollo
+Pechuga asada, jamón, queso mozzarella, vegetales y queso.
+Precio: $15.000
+
+Ranchero
+Carne artesanal, salchicha ranchera, tocineta, queso mozzarella, vegetales y queso.
+Precio: $20.000
+
+Mixto
+Pechuga asada, carne artesanal, tocineta, queso mozzarella, vegetales y queso.
+Precio: $22.000
+
+Doble Carne
+Carne artesanal, tocineta, queso mozzarella, vegetales y queso.
+Precio: $22.000
+
+Estacionario
+Carne artesanal, pechuga asada, salchicha ranchera, tocineta, maíz, queso mozzarella, vegetales y queso.
+Precio: $25.000
+
+HOT DOG
+
+Sencillo + Papas
+Salchicha, queso, vegetales, salsa + papa y queso.
+Precio: $10.000
+
+Chori Perro
+Chorizo, jamón, quesillo, vegetales, salsa + papa y queso.
+Precio: $15.000
+
+Perro Ranchero
+Salchicha ranchera, tocineta, jamón, quesillo, vegetales, salsa + papa y queso.
+Precio: $18.000
+
+Estacionario
+Chorizo, pollo, tocineta, maíz tierno, jamón, queso mozzarella, papa francesa y salsa.
+Precio: $24.000
+
+MARÍA CASQUITO
+
+Sencillo
+Salchicha, chorizo, butifarra, cebolla a la grille, lechuga, salsa y queso.
+Precio: $20.000
+
+Especial
+Carne, pechuga, salchicha ranchera, jamón, cebolla a la grille, lechuga, salsa y queso.
+Precio: $25.000
+
+PATACÓN RELLENO
+
+Porción de Patacones
+Patacones + queso.
+Precio: $6.000
+
+Sencillo
+Salchicha, chorizo, butifarra, cebolla a la grille, lechuga, salsa y queso.
+Precio: $20.000
+
+Especial
+Carne, pechuga, chorizo, butifarra, lomo de cerdo, jamón, cebolla a la grille, vegetales, queso y salsa.
+Precio: $25.000
+
+DESGRANADOS
+
+Sencillo
+Salchicha, butifarra, chorizo, maíz, vegetales, queso y salsa.
+Precio: $20.000
+
+Especial
+Maíz tierno, salchicha ranchera, chorizo, butifarra, carne, pechuga, vegetales, queso mozzarella.
+Precio: $25.000
+
+Estacionario (2 personas)
+Maíz tierno, salchicha ranchera, chorizo, butifarra, carne, pechuga, vegetales, queso mozzarella.
+Precio: $40.000
+
+SANDWICH
+
+Sencillo
+Jamón, queso mozzarella, vegetales y salsa.
+Precio: $6.000
+
+De Pollo
+Pechuga, jamón, queso mozzarella, vegetales, salsa + porción de papa.
+Precio: $15.000
+
+De Carne
+Carne, jamón, queso mozzarella, vegetales, salsa + porción de papa.
+Precio: $15.000
+
+Especial Estacionario
+Carne, pechuga, salchicha ranchera, maíz, jamón, queso mozzarella, vegetales, salsa + porción de papa.
+Precio: $25.000
+
+BEBIDAS
+
+Gaseosa
+
+Agua
+
+Jugo Hit
+
+Pony Malta
+
+Cerveza
+
+Cerveza Escarchada
+
+Jugos Naturales
+
+Limonada Natural
+
+Limonada Cerezada
+
+Micheladas
+
+Malteadas
+
 - Horario: 12:00 PM a 10:00 PM
 - Ubicación: Calle 10 #9-133, La Loma, El Paso, Cesar
 - Teléfono: +573113509246
